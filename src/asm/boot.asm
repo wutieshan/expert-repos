216a4a57fc02de49e9f75f76fa14b7953daf9e07
wutieshan <1766501249@qq.com>
2025-03-07 00:04:18 +0800

    tieshan

--- src/asm/boot.asm
+++ src/asm/boot.asm
@@ -1,39 +1,27 @@
-; org 0x7c00
+org 0x7c00
 
 
-; start:
-;     mov edx, len
-;     mov ecx, msg
-;     mov ebx, 1
-;     mov eax, 4
-;     int 0x80
+start:
+    mov ax, 0x03
+    int 0x10
 
-;     mov edx, 9
-;     mov ecx, s2
-;     mov ebx, 1
-;     mov eax, 4
-;     int 0x80
-
-;     mov eax, 1
-;     int 0x80
+    mov si, msg
+    call print
 
 
-; msg db 'Displaying 9 stars', 0xa
-; len equ $ - msg
-; s2 times 9 db '*'
-; times 510 - ($ - $$) db 0
-; dw 0xaa55
+print:
+    mov ah, 0x0e
+
+    .loop:
+        lodsb
+        cmp al, 0
+        je .done
+        int 0x10
+        jmp .loop
+    .done:
+        ret
 
 
-
-section .text
-    global _start
-
-
-_start:
-    mov r0, #10
-    mov r1, #20
-    add r2, r0, r1
-
-    mov r7, #1
-    swi 0x0
+msg db "tieshan, wu...", 0
+times 510 - ($ - $$) db 0
+db 0x55, 0xaa
