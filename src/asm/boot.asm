89af61561993230dbe5371d132fe57cc20d89181
wutieshan <1766501249@qq.com>
2025-03-11 18:51:21 +0800

    tieshan

--- src/asm/boot.asm
+++ src/asm/boot.asm
@@ -1,26 +1,8 @@
-section .text
-    global start
 start:
-    mov ax, 0x03
+    mov ax, 0x07
+    mov ah, 0x0e
     int 0x10
 
-    mov si, msg
-    call print
 
-
-print:
-    mov ah, 0x0e
-
-    .loop:
-        lodsb
-        cmp al, 0
-        je .done
-        int 0x10
-        jmp .loop
-    .done:
-        ret
-
-
-msg db "tieshan, wu...", 0
 times 510 - ($ - $$) db 0
-db 0x55, 0xaa
+dw 0xaa55
